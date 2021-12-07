6daec3c90e45f9df44d1948e5d9fd04614ffbf40
nill <nill@mail.chungyo.net>
2021-12-07 09:43:10 +0800

    two

--- AB.js
+++ AB.js
@@ -14,23 +14,26 @@ btn.addEventListener("click", function(){
     xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
     xhr.send(jsn);
     xhr.onload = function() {
-        console.log(rangestr);
-        console.log(this.responseText);
-        obj=JSON.parse(this.responseText);
-        let objlength = obj.length-1;
-        answer.innerText = obj[objlength].result;
-        obj.forEach(element => {
-            if(element.rangestr=="1")
-                rangestr=1;
-            else
-                rangestr=0;
-            answer.innerText=element.result;
-            if(element.num ==1)
-                history.innerText = "輸入： " + element.replied + " 結果： " +element.result +"\n";
-            else
-                history.innerText += "輸入： " + element.replied + " 結果： " +element.result +"\n";
-            
-        });
+        try{
+            obj=JSON.parse(this.responseText);
+            let objlength = obj.length-1;
+            answer.innerText = obj[objlength].result;
+            obj.forEach(element => {
+                if(element.rangestr=="1")
+                    rangestr=1;
+                else
+                    rangestr=0;
+                answer.innerText=element.result;
+                if(element.num ==1)
+                    history.innerText = "輸入： " + element.replied + " 結果： " +element.result +"\n";
+                else
+                    history.innerText += "輸入： " + element.replied + " 結果： " +element.result +"\n";
+                
+            });
+        }catch(e){
+            answer.innerText = this.response;
+        }
+        
     }
     
     
